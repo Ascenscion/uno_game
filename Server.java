cf0a513d98ff1637aaf1b8ae75a9bb6034c19b98
Xavier Ortiz <charlesxo17@gmail.com>
2024-05-26 16:23:45 -0700

    Cambio de formato

--- Server.java
+++ Server.java
@@ -12,7 +12,7 @@ import java.awt.event.ActionEvent;
 public class Server extends JFrame {
   private ServerSocket serverSocket;
 
-  private static ArrayList <String> apodos;
+  private static ArrayList<String> apodos;
   private static ArrayList<Integer> numCartasJugadores = new ArrayList<>();
   private static int contadorJugadores = 0;
   private boolean comenzar = false;
@@ -30,79 +30,80 @@ public class Server extends JFrame {
     try {
       System.out.println("IP: " + InetAddress.getLocalHost().getHostAddress());
       System.out.println("Puerto: " + ss.getLocalPort());
-    }
-    catch(Exception e) {
+    } catch (Exception e) {
       e.printStackTrace();
     }
     System.out.println("[ Servidor Iniciado correctamente ]");
 
     getContentPane().setBackground(Color.red);
-    ImageIcon icono = new ImageIcon ("iconos/logo.png");
+    ImageIcon icono = new ImageIcon("iconos/logo.png");
     JLabel icono_UNO = new JLabel(icono);
     add(icono_UNO, BorderLayout.NORTH);
-    
+
     JPanel panel = new JPanel(new BorderLayout());
     panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
     panel.setOpaque(false);
     add(panel, BorderLayout.CENTER);
-    
-    iniciar = new JButton ("Iniciar");
+
+    iniciar = new JButton("Iniciar");
     iniciar.setBorder(BorderFactory.createLineBorder(Color.black, 3));
     iniciar.setFont(new java.awt.Font("Segoe UI", 3, 58));
     iniciar.setBackground(Color.yellow);
     // Boton iniciar
     iniciar.addActionListener((ActionEvent evento) -> {
-        if(contadorJugadores >= 1){
-            Random rand = new Random();
-            baraja = Card.generarBaraja();
-            Card cartaInicial = baraja.pop();
-            cartaInicial.removeMouseListener(cartaInicial);
-            if(cartaInicial.getColorInt() == CardColor.BLACK)
-              cartaInicial.setColor(new CardColor(rand.nextInt(4)));
-            comenzar = true;
+      if (contadorJugadores >= 1) {
+        Random rand = new Random();
+        baraja = Card.generarBaraja();
+        Card cartaInicial = baraja.pop();
+        cartaInicial.removeMouseListener(cartaInicial);
+        if (cartaInicial.getColorInt() == CardColor.BLACK)
+          cartaInicial.setColor(new CardColor(rand.nextInt(4)));
+        comenzar = true;
 
-            PacketData paqueteEnviar = new PacketData();
-            paqueteEnviar.globalNumCartas = new ArrayList<>(contadorJugadores);
-            // Rellenar el numero de cartas de cada jugador
-            for(int n = 0; n < contadorJugadores; n++) {
-              paqueteEnviar.globalNumCartas.add(numCartasIniciales);
-              numCartasJugadores.add(numCartasIniciales);
-            }
-            // Se realiaza un for porque se necesita enviarle a cada cliente 7 cartas de la baraja distintas a cada uno (si se utiliza broadcastFromServer se enviarian las 7 mismas cartas a todos los jugadores)
-            for(int i = 0; i < contadorJugadores; i++) {
-              paqueteEnviar.barajaCartas = Card.comerCartas(baraja, numCartasIniciales);
-              paqueteEnviar.cartaInicial = cartaInicial;
-              paqueteEnviar.accion = ServerAction.START;
-              paqueteEnviar.turno = 0;
-              paqueteEnviar.direccion = direccion;
-              paqueteEnviar.nombre = apodos.get(i);
-              paqueteEnviar.apodosJugadores = apodos;
-
-              //Envia el paquete a todos los clientes
-              ClientHandler.sendPacketToClientFromServer(paqueteEnviar, i);
-            }
-            setVisible(false);
-
-            // Panel de Pruebas para Debug 
-            //SwingUtilities.invokeLater(()-> { new PanelDebug(this); });
-
-        } else {
-            JOptionPane.showMessageDialog(null, "Necesitas al menos dos jugadores + [ " + contadorJugadores + " ]", 
-            "Error de inicio", JOptionPane.ERROR_MESSAGE);
+        PacketData paqueteEnviar = new PacketData();
+        paqueteEnviar.globalNumCartas = new ArrayList<>(contadorJugadores);
+        // Rellenar el numero de cartas de cada jugador
+        for (int n = 0; n < contadorJugadores; n++) {
+          paqueteEnviar.globalNumCartas.add(numCartasIniciales);
+          numCartasJugadores.add(numCartasIniciales);
         }
+        // Se realiaza un for porque se necesita enviarle a cada cliente 7 cartas de la
+        // baraja distintas a cada uno (si se utiliza broadcastFromServer se enviarian
+        // las 7 mismas cartas a todos los jugadores)
+        for (int i = 0; i < contadorJugadores; i++) {
+          paqueteEnviar.barajaCartas = Card.comerCartas(baraja, numCartasIniciales);
+          paqueteEnviar.cartaInicial = cartaInicial;
+          paqueteEnviar.accion = ServerAction.START;
+          paqueteEnviar.turno = 0;
+          paqueteEnviar.direccion = direccion;
+          paqueteEnviar.nombre = apodos.get(i);
+          paqueteEnviar.apodosJugadores = apodos;
+
+          // Envia el paquete a todos los clientes
+          ClientHandler.sendPacketToClientFromServer(paqueteEnviar, i);
+        }
+        setVisible(false);
+
+        // Panel de Pruebas para Debug
+        // SwingUtilities.invokeLater(()-> { new PanelDebug(this); });
+
+      } else {
+        JOptionPane.showMessageDialog(null, "Necesitas al menos dos jugadores + [ " + contadorJugadores + " ]",
+            "Error de inicio", JOptionPane.ERROR_MESSAGE);
+      }
     });
     //
     panel.add(iniciar, BorderLayout.CENTER);
-    setSize(500,350);
+    setSize(500, 350);
     setVisible(true);
-    }
+  }
 
   public void startServer() {
     try {
-      while(!serverSocket.isClosed()) {
+      while (!serverSocket.isClosed()) {
         Socket socket = serverSocket.accept();
-        if(!comenzar && contadorJugadores < 4){
-          contadorJugadores ++;
+        if (!comenzar && contadorJugadores < 4) {
+          contadorJugadores++;
           System.out.println("Cliente conectado " + socket.getInetAddress().getHostAddress());
           System.out.println("SERVER> Total de jugadores conectados: " + contadorJugadores);
           ClientHandler clientHandler = new ClientHandler(socket, contadorJugadores);
@@ -113,19 +114,17 @@ public class Server extends JFrame {
           socket.close();
         }
       }
-    }
-    catch(Exception e) {
+    } catch (Exception e) {
       e.printStackTrace();
     }
   }
 
   public void closeServerSocket() {
     try {
-      if(serverSocket != null) {
+      if (serverSocket != null) {
         serverSocket.close();
       }
-    }
-    catch(Exception e) {
+    } catch (Exception e) {
       e.printStackTrace();
     }
   }
@@ -136,133 +135,136 @@ public class Server extends JFrame {
     server.startServer();
   }
 
-  // Importante: no modificar directamente los objetos del paquete recibido, en cambio crear uno nuevo y reemplazarlo
-  // Nota: Siempre crear una nueva instancia de un ArrayList si se quiere modificar la informacion del paquete
-  public static PacketData receiveClientMovement(PacketData Movement){
-    //System.out.println("Paquete recibido de " + Movement.nombre + "\n" + Movement);
-      switch(Movement.accion){
-        case NEW_ELEMENTS:
-          apodos.add(Movement.nombre);
-          Movement.apodosJugadores = (ArrayList<String>) apodos.clone();
+  // Importante: no modificar directamente los objetos del paquete recibido, en
+  // cambio crear uno nuevo y reemplazarlo
+  // Nota: Siempre crear una nueva instancia de un ArrayList si se quiere
+  // modificar la informacion del paquete
+  public static PacketData receiveClientMovement(PacketData Movement) {
+    // System.out.println("Paquete recibido de " + Movement.nombre + "\n" +
+    // Movement);
+    switch (Movement.accion) {
+      case NEW_ELEMENTS:
+        apodos.add(Movement.nombre);
+        Movement.apodosJugadores = (ArrayList<String>) apodos.clone();
         break;
 
-        case THROW_CARD:
-          Card carta = Movement.cartaDeCliente.copy(false);
-          if(Movement.numCartas == 0) {
-            System.out.println(Movement.nombre + " Gano");
-            Movement.accion = ServerAction.END;
-            return Movement;
-          }
+      case THROW_CARD:
+        Card carta = Movement.cartaDeCliente.copy(false);
+        if (Movement.numCartas == 0) {
+          System.out.println(Movement.nombre + " Gano");
+          Movement.accion = ServerAction.END;
+          return Movement;
+        }
 
-          // Reverso
-          if(carta.getCardType() == CardType.REVERSE)
-            direccion *= -1;
-          // Reverso y bloqueo
-          if(carta.getCardType() == CardType.REVERSE || carta.getCardType() == CardType.BLOCK)
-            siguienteTurno();
-          
+        // Reverso
+        if (carta.getCardType() == CardType.REVERSE)
+          direccion *= -1;
+        // Reverso y bloqueo
+        if (carta.getCardType() == CardType.REVERSE || carta.getCardType() == CardType.BLOCK)
           siguienteTurno();
-          numCartasJugadores.set(Movement.turno, Movement.numCartas);
-          Movement.apodosJugadores = (ArrayList<String>) apodos.clone();
-          Movement.globalNumCartas = new ArrayList<>(numCartasJugadores);
-          Movement.turno = jugadorActual;
-          Movement.direccion = direccion;
-          Movement.accion = ServerAction.UPDATE_INFO;
-          
-          // Si la carta tirada es un +4 o +2
-          if(carta.getCardType() == CardType.WILD_EAT || carta.getCardType() == CardType.EAT) {
-            // primero le actualizas la informacion al cliente de que ya es su turno
-            ClientHandler.sendPacketToClientFromServer(Movement, jugadorActual);
-            // Despues le mandas a comer
-            PacketData paqueteComer = new PacketData();
-            paqueteComer.nombre = new String(Movement.nombre);
-            paqueteComer.accion = ServerAction.EAT;
-            paqueteComer.turno = jugadorActual;
-            paqueteComer.cartaDeCliente = Movement.cartaDeCliente.copy(true);
-            paqueteComer.direccion = direccion;
-            paqueteComer.globalNumCartas = new ArrayList<>(numCartasJugadores);
-            paqueteComer.apodosJugadores = (ArrayList<String>) apodos.clone();
-            if(baraja.size() <= 4)
-              baraja = Card.generarBaraja();
-            int cartasAComer = carta.getCardType() == CardType.WILD_EAT ? 4 : 2;
-            
-            LinkedList<Card> cartasEnviadas = new LinkedList<>(Movement.barajaCartas); // aqui hay un problema
-            // Se suman las cartas
-            cartasEnviadas.addAll(Card.comerCartas(baraja, cartasAComer));
 
-            paqueteComer.barajaCartas = new LinkedList<>(cartasEnviadas);
-            ClientHandler.sendPacketToClientFromServer(paqueteComer, jugadorActual);
-          }
-        break;
+        siguienteTurno();
+        numCartasJugadores.set(Movement.turno, Movement.numCartas);
+        Movement.apodosJugadores = (ArrayList<String>) apodos.clone();
+        Movement.globalNumCartas = new ArrayList<>(numCartasJugadores);
+        Movement.turno = jugadorActual;
+        Movement.direccion = direccion;
+        Movement.accion = ServerAction.UPDATE_INFO;
 
-        // Pasar turno
-        case PASS:
-        System.out.println("Jugador actual: " + apodos.get(jugadorActual) + " vs Juagador recibido: " + Movement.nombre);
-          siguienteTurno();
-          numCartasJugadores.set(Movement.turno, Movement.numCartas);
-          Movement.apodosJugadores = (ArrayList<String>) apodos.clone();
-          Movement.globalNumCartas = new ArrayList<>(numCartasJugadores);
-          Movement.turno = jugadorActual;
-          Movement.direccion = direccion;
-          Movement.accion = ServerAction.UPDATE_INFO;
-        break;
-
-        case UPDATE_INFO:
-          numCartasJugadores.set(Movement.turno, Movement.numCartas);
-          Movement.apodosJugadores = (ArrayList<String>) apodos.clone();
-          Movement.globalNumCartas = new ArrayList<>(numCartasJugadores);
-          Movement.turno = jugadorActual;
-          Movement.direccion = direccion;
-          Movement.accion = ServerAction.UPDATE_INFO;
-        break;
-  
-        case EAT:
-          if(!baraja.isEmpty())
+        // Si la carta tirada es un +4 o +2
+        if (carta.getCardType() == CardType.WILD_EAT || carta.getCardType() == CardType.EAT) {
+          // primero le actualizas la informacion al cliente de que ya es su turno
+          ClientHandler.sendPacketToClientFromServer(Movement, jugadorActual);
+          // Despues le mandas a comer
+          PacketData paqueteComer = new PacketData();
+          paqueteComer.nombre = new String(Movement.nombre);
+          paqueteComer.accion = ServerAction.EAT;
+          paqueteComer.turno = jugadorActual;
+          paqueteComer.cartaDeCliente = Movement.cartaDeCliente.copy(true);
+          paqueteComer.direccion = direccion;
+          paqueteComer.globalNumCartas = new ArrayList<>(numCartasJugadores);
+          paqueteComer.apodosJugadores = (ArrayList<String>) apodos.clone();
+          if (baraja.size() <= 4)
             baraja = Card.generarBaraja();
-          Card c = baraja.pop();
-          numCartasJugadores.set(Movement.turno, Movement.numCartas);
-          Movement.globalNumCartas = new ArrayList<>(numCartasJugadores);
+          int cartasAComer = carta.getCardType() == CardType.WILD_EAT ? 4 : 2;
 
-          PacketData nuevoPaquete = new PacketData();
-          nuevoPaquete.turno = Movement.turno;
-          nuevoPaquete.accion = ServerAction.EAT;
-          nuevoPaquete.nombre = "Servidor";
-          nuevoPaquete.cartaDeCliente = c.copy(true);
-          nuevoPaquete.apodosJugadores = (ArrayList<String>) apodos.clone();
-          nuevoPaquete.globalNumCartas = new ArrayList<Integer>(numCartasJugadores);
-          ClientHandler.sendPacketToClientFromServer(nuevoPaquete, nuevoPaquete.turno);
+          LinkedList<Card> cartasEnviadas = new LinkedList<>(Movement.barajaCartas); // aqui hay un problema
+          // Se suman las cartas
+          cartasEnviadas.addAll(Card.comerCartas(baraja, cartasAComer));
 
-          // Si el jugador tenia una carta valida para poner, pero decidio comer entonces se pasa su turno
-          for(Card card : Movement.barajaCartas) {
-            if(card.isValid(Movement.cartaDeCliente)) {
-              siguienteTurno();
-              break;
-            }
+          paqueteComer.barajaCartas = new LinkedList<>(cartasEnviadas);
+          ClientHandler.sendPacketToClientFromServer(paqueteComer, jugadorActual);
+        }
+        break;
+
+      // Pasar turno
+      case PASS:
+        System.out
+            .println("Jugador actual: " + apodos.get(jugadorActual) + " vs Juagador recibido: " + Movement.nombre);
+        siguienteTurno();
+        numCartasJugadores.set(Movement.turno, Movement.numCartas);
+        Movement.apodosJugadores = (ArrayList<String>) apodos.clone();
+        Movement.globalNumCartas = new ArrayList<>(numCartasJugadores);
+        Movement.turno = jugadorActual;
+        Movement.direccion = direccion;
+        Movement.accion = ServerAction.UPDATE_INFO;
+        break;
+
+      case UPDATE_INFO:
+        numCartasJugadores.set(Movement.turno, Movement.numCartas);
+        Movement.apodosJugadores = (ArrayList<String>) apodos.clone();
+        Movement.globalNumCartas = new ArrayList<>(numCartasJugadores);
+        Movement.turno = jugadorActual;
+        Movement.direccion = direccion;
+        Movement.accion = ServerAction.UPDATE_INFO;
+        break;
+
+      case EAT:
+        if (!baraja.isEmpty())
+          baraja = Card.generarBaraja();
+        Card c = baraja.pop();
+        numCartasJugadores.set(Movement.turno, Movement.numCartas);
+        Movement.globalNumCartas = new ArrayList<>(numCartasJugadores);
+
+        PacketData nuevoPaquete = new PacketData();
+        nuevoPaquete.turno = Movement.turno;
+        nuevoPaquete.accion = ServerAction.EAT;
+        nuevoPaquete.nombre = "Servidor";
+        nuevoPaquete.cartaDeCliente = c.copy(true);
+        nuevoPaquete.apodosJugadores = (ArrayList<String>) apodos.clone();
+        nuevoPaquete.globalNumCartas = new ArrayList<Integer>(numCartasJugadores);
+        ClientHandler.sendPacketToClientFromServer(nuevoPaquete, nuevoPaquete.turno);
+
+        // Si el jugador tenia una carta valida para poner, pero decidio comer entonces
+        // se pasa su turno
+        for (Card card : Movement.barajaCartas) {
+          if (card.isValid(Movement.cartaDeCliente)) {
+            siguienteTurno();
+            break;
           }
-          Movement.cartaDeCliente = c.copy(true);
-          Movement.turno = jugadorActual;
-          Movement.nombre = "Servidor";
-          Movement.accion = ServerAction.UPDATE_INFO;
+        }
+        Movement.cartaDeCliente = c.copy(true);
+        Movement.turno = jugadorActual;
+        Movement.nombre = "Servidor";
+        Movement.accion = ServerAction.UPDATE_INFO;
         break;
-  
-        case UNO:
-        System.out.println("8");                    
+
+      case UNO:
+        System.out.println("8");
         break;
-  
-        default:
-            System.out.println("9");
+
+      default:
+        System.out.println("9");
         break;
-      }
+    }
     return Movement;
   }
 
   public static void siguienteTurno() {
     jugadorActual += direccion;
-    if(jugadorActual >= contadorJugadores)
+    if (jugadorActual >= contadorJugadores)
       jugadorActual = 0;
-    if(jugadorActual < 0)
-      jugadorActual = contadorJugadores-1;
+    if (jugadorActual < 0)
+      jugadorActual = contadorJugadores - 1;
   }
 }
-
-  
